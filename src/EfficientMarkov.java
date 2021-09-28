af317cd3abe6b26fbfc8cb5429fc165ae5afa051
Samantha Owusu-Antwi <samantha.owusuantwi@duke.edu>
2021-09-27 23:38:54 -0400

    rewrite setTraining()

--- src/EfficientMarkov.java
+++ src/EfficientMarkov.java
@@ -18,25 +18,30 @@ public class EfficientMarkov extends BaseMarkov {
 		myMap.clear();
 
 		for(int i=0; i < myText.length() - myOrder + 1; i++){
-			String key = myText.substring(i, i+myOrder);	// note: .substring() truncates at i+myOrder so value at i+myOrder is not included in substring
+			String key_ = myText.substring(i, i+myOrder);	// note: .substring() truncates at i+myOrder so value at i+myOrder is not included in substring
 
 			//String value = PSEUDO_EOS;
-			String value = "";
+			//String value = "";
 
 			//if(i+myOrder == myText.length()){	// if the characters are the last characters of the string they will have no following characters
 			//	String value = PSEUDO_EOS;
 			//} else {
-			if(i+myOrder+1 < myText.length()){
-				value = myText.substring(i + myOrder /*+ 1*/, i + myOrder + 1/*2*/); // get the letter that occurs next
+			myMap.putIfAbsent(key_, new ArrayList<String>());
+
+			//if(i+myOrder+1 < myText.length()){
+			if(i+myOrder == myText.length()){
+				myMap.get(key_).add(PSEUDO_EOS);
 			} else {
-				value = PSEUDO_EOS;
+				String value = myText.substring(i + myOrder /*+ 1*/, i + myOrder + 1/*2*/); // get the letter that occurs next
+				myMap.get(key_).add(value);
 			}
 
-			myMap.putIfAbsent(key, new ArrayList<String>());
-			ArrayList<String> current_value = myMap.get(key);
-			current_value.add(value);
+			//myMap.putIfAbsent(key_, new ArrayList<String>());
+			//ArrayList<String> current_value = myMap.get(key_);
+			//current_value.add(value);
 			//myMap.put(key, myMap.get(key).add(value));
-			myMap.put(key, current_value);
+			//myMap.put(key_, current_value);
+			//System.out.println(myMap);
 		}
 	}
 	@Override
